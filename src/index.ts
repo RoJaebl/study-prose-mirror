11968a1c2e3738de216c32ff41da93eba18cac4a
RoJaebl <qgs0924@gmail.com>
2025-05-13 18:14:20 +0900

    :recycle: refactor: DOM 파싱을 통한 초기 콘텐츠 설정
    - HTML에 초기 콘텐츠용 DOM 요소 추가
    - DOMParser를 사용하여 초기 콘텐츠 파싱 구현
    - 불필요한 플러그인 관련 코드 제거

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,13 @@ import { schema } from "prosemirror-schema-basic";
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import "prosemirror-view/style/prosemirror.css";
-import { undo, redo, history } from "prosemirror-history";
-import { keymap } from "prosemirror-keymap";
-import { baseKeymap } from "prosemirror-commands";
-import { exampleSetup } from "prosemirror-example-setup";
-import "prosemirror-menu/style/menu.css";
+import { DOMParser } from "prosemirror-model";
 
+let content = document.getElementById("content"); // DOM 노드를 쿼리한다.
 let state = EditorState.create({
-  schema,
-  // plugins: [history(), keymap({ "Mod-z": undo, "Mod-y": redo, ...baseKeymap })],
-  plugins: [...exampleSetup({ schema })],
+  // doc 요소를 추가할때 schema 요소를 생략해도 된다.
+  // schema: schema
+  // DOMParser를 이용하여 스키마를 따르는 문서 파서를 생성하고 쿼리한 DOM 노드를 ProseMirror 요소로 파싱한다.
+  doc: DOMParser.fromSchema(schema).parse(content!),
 });
 let view = new EditorView(document.querySelector("#editor")!, { state });

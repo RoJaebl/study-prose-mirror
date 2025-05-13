f255d365a69e0b5477958a0cc52782eee7b37d83
RoJaebl <qgs0924@gmail.com>
2025-05-13 17:45:54 +0900

    :heavy_plus_sign: feat: 에디터 메뉴 추가
    - prosemirror-commands, prosemirror-menu 의존성 추가
    - prosemirror-example-setup devDependency 추가
    - example setup 플러그인으로 변경하여 메뉴 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { EditorView } from "prosemirror-view";
 import "prosemirror-view/style/prosemirror.css";
 import { undo, redo, history } from "prosemirror-history";
 import { keymap } from "prosemirror-keymap";
+import { baseKeymap } from "prosemirror-commands";
+import { exampleSetup } from "prosemirror-example-setup";
+import "prosemirror-menu/style/menu.css";
 
-/**
- * 플러그인을 state를 생성할 때 등록하여 사용할 수 있다.
- */
 let state = EditorState.create({
   schema,
-  plugins: [history(), keymap({ "Mod-z": undo, "Mod-y": redo })],
+  // plugins: [history(), keymap({ "Mod-z": undo, "Mod-y": redo, ...baseKeymap })],
+  plugins: [...exampleSetup({ schema })],
 });
 let view = new EditorView(document.querySelector("#editor")!, { state });
